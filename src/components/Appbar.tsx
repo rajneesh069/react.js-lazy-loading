007ca1d7331c2d87026b525f7a526f5cb81e7a94
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-03 09:56:05 +0530

    everything is working, now going to add lazy loading

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,8 +3,17 @@ import { useNavigate } from "react-router-dom";
 export default function Appbar() {
   const navigate = useNavigate();
   return (
-    <div style={{ background: "black", color: "white" }}>
-      This is the Appbar.
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        display: "flex",
+        gap: 5,
+        justifyContent: "center",
+        padding: 3,
+      }}
+    >
+      <p>This is the Appbar.</p>
       <button
         onClick={() => {
           navigate("/dashboard");

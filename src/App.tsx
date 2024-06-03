71077156a74873ea09ac31bfc5513ee0ff6a0987
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-03 09:53:53 +0530

    add appbar to under the BrowserRouter component but outside routes so that it shows on every page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Landing from "./components/Landing";
+import Appbar from "./components/Appbar";
 
 function App() {
   return (
-    <>
+    <div>
       <BrowserRouter>
+        <Appbar />
         <Routes>
           <Route element={<Dashboard />} path="/dashboard" />
           <Route element={<Landing />} path="/" />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 

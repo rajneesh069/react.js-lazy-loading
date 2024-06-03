a8d268784b8ac4f098e75b6f9675bd2960bba398
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-03 10:04:59 +0530

    add loading component and finsish lazy loading in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import Landing from "./components/Landing";
+import { lazy, Suspense } from "react";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Landing = lazy(() => import("./components/Landing"));
 import Appbar from "./components/Appbar";
-
+import Loading from "./components/Loading";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Appbar />
         <Routes>
-          <Route element={<Dashboard />} path="/dashboard" />
-          <Route element={<Landing />} path="/" />
+          <Route
+            element={
+              <Suspense fallback={<Loading />}>
+                <Dashboard />
+              </Suspense>
+            }
+            path="/dashboard"
+          />
+          <Route
+            element={
+              <Suspense fallback={<Loading />}>
+                <Landing />
+              </Suspense>
+            }
+            path="/"
+          />
         </Routes>
       </BrowserRouter>
     </div>

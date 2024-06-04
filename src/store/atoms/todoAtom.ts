0e9356f36b00e0cb6e7d43beb727f9e9b485df95
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-04 16:42:43 +0530

    asynchrounous queries in atomFamily

--- src/store/atoms/todoAtom.ts
+++ src/store/atoms/todoAtom.ts
@@ -1,5 +1,5 @@
-import { atom, atomFamily } from "recoil";
-import { TODOS } from "../../components/Todos";
+import { atom, atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
 
 export interface todo {
   id: string | null;
@@ -18,9 +18,30 @@ export const searchTodoAtom = atom<todo["title"]>({
 });
 
 //atom family
+//separate atom is needed because in case of an array of todos if one element changes
+//the whole atom will refresh triggering the re-render of every todo while
+//in case of atomFamily only that piece will re-render as they have separate atoms!
+// export const todosAtoms = atomFamily({
+//   key: "todosAtoms",
+//   default: (id) => {
+//     return TODOS.find((todo) => todo.id === id);
+//   },
+// });
+
 export const todosAtoms = atomFamily({
   key: "todosAtoms",
-  default: (id) => {
-    return TODOS.find((todo) => todo.id === id);
-  },
+  default: selectorFamily({
+    key: "todoAtoms/default",
+    get: (id) => async () => {
+      const response = await axios.get(
+        `http://localhost:8080/todo?id=${String(id)}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    },
+  }),
 });
